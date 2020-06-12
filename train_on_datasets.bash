20d6282920f94ce267077d966da5259abb3a81bc
enesozi <enes_ozipek94@hotmail.com>
2020-06-12 19:12:55 +0300

    weight link changed

--- train_on_datasets.bash
+++ train_on_datasets.bash
@@ -4,10 +4,9 @@ args=("$@")
 python validate_and_configure.py
 
 # Yolov3.conv.81 required for finetuning 
-if [[ ! -f yolov3.conv.81 ]]
-  then
-    megadl 'https://mega.nz/#!qk9mgYwJ!5Ndff-Z9ZdKifZpY9Qu9WeHQVBeDiCwZbzSn_fJhjkc'
-  fi
+if [[ ! -f yolov3.conv.81 ]] then
+    wget ${args[0]} 
+fi
 
 # Start training
 CUDA_LAUNCH_BLOCKING=1 python train.py --cfg yolov3_custom.cfg --data custom.data  --epochs 100 --weights yolov3.conv.81 --multi-scale
