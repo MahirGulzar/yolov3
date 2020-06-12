b4432747bfec0ce3e4ee68d0c9da358fad57faff
enesozi <enes_ozipek94@hotmail.com>
2020-06-12 19:12:55 +0300

    weight link changed

--- train_on_datasets.bash
+++ train_on_datasets.bash
@@ -6,7 +6,7 @@ python validate_and_configure.py
 # Yolov3.conv.81 required for finetuning 
 if [[ ! -f yolov3.conv.81 ]]
   then
-    megadl 'https://mega.nz/#!qk9mgYwJ!5Ndff-Z9ZdKifZpY9Qu9WeHQVBeDiCwZbzSn_fJhjkc'
+    wget ${args[0]}
   fi
 
 # Start training
