f39f82a8c577766b7b11fca0d297bda78d1a5d0b
enesozi <enes_ozipek94@hotmail.com>
2020-06-15 11:32:23 +0300

    checkpoint dir added

--- train_on_datasets.bash
+++ train_on_datasets.bash
@@ -1,5 +1,9 @@
 args=("$@")
 
+# Checkpoint directory
+CHKPT_DIR=/home/data/Train_Unified_Yolo/checkpoints_$(date +%Y-%m-%d:%H:%M:%S)
+mkdir -p $CHKPT_DIR
+
 # Validate and Configure the training
 python validate_and_configure.py
 
@@ -10,4 +14,7 @@ if [[ ! -f yolov3.conv.81 ]]
   fi
 
 # Start training
-CUDA_LAUNCH_BLOCKING=1 python train.py --cfg yolov3_custom.cfg --data custom.data  --epochs 100 --weights yolov3.conv.81 --multi-scale
+CUDA_LAUNCH_BLOCKING=1 python train.py --cfg yolov3_custom.cfg --data custom.data  --epochs 1 --weights yolov3.conv.81 --multi-scale
+
+cp -r runs $CHKPT_DIR/
+cp -r weights $CHKPT_DIR/
