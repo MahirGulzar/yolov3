313576acb060581a52434e65d0fe9a6837b43ce2
Hannes Liik <hannes_liik@hotmail.com>
2020-06-16 13:32:44 +0300

    Add script to run trained model to output for train_unified

--- train_unified.bash
+++ train_unified.bash
@@ -7,7 +7,9 @@ mkdir -p $CHKPT_DIR
 python validate_and_configure.py
 
 # Start training
-python train.py --cfg yolov3_custom.cfg --data custom.data --epochs 1 --weights yolov3.conv.81 --multi-scale --use_seg_depth --rect --notest
+python train.py --cfg yolov3_custom.cfg --data custom.data --epochs 40 --weights yolov3.conv.81 --multi-scale --use_seg_depth --rect --notest
 
 cp -r runs $CHKPT_DIR/
 cp weights/last.pt $CHKPT_DIR/yolov3_unified_semseg.pt
+
+echo "segment_images --segmenter yolo_generic --model_path yolov3_unified_semseg.pt --source_labelspace milrem $1 $2" > $CHKPT_DIR/inference.sh
