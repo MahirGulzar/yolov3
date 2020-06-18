592a4c8f1a1a52b5da669fa784e60fab58158dcc
Hannes Liik <hannes_liik@hotmail.com>
2020-06-18 14:40:11 +0300

    Test model naming

--- train_unified.bash
+++ train_unified.bash
@@ -3,10 +3,12 @@ set -e
 
 # Checkpoint directory
 CHKPT_DIR=/home/data/unified_yolo_checkpoints/checkpoints_$(date +%Y-%m-%d:%H:%M:%S)
-mkdir -p $CHKPT_DIR
+mkdir -p /home/checkpoint_dir
+data_path=$1
+model_name=$2
 
 # Validate and Configure the training
-python validate_and_configure.py $CHKPT_DIR --data_path $1 
+python validate_and_configure.py /home/checkpoint_dir --data_path $1 
 
 shift
 
@@ -16,9 +18,9 @@ shift
 # --seg_classes to set how many classes segmentation has
 
 # Start training
-python train.py --cfg yolov3_custom.cfg --data custom.data --epochs 40 --weights yolov3.conv.81 --multi-scale --rect --notest $@
+python train.py --cfg yolov3_custom.cfg --data custom.data --epochs 2 --weights yolov3.conv.81 --multi-scale --rect --notest $@
 
-cp -r runs $CHKPT_DIR/
-cp weights/last.pt $CHKPT_DIR/yolov3_unified_semseg.pt
+cp -r runs /home/checkpoint_dir/
+cp weights/last.pt /home/checkpoint_dir/$model_name.pt
 
-echo 'segment_images --segmenter yolo_generic --model_path yolov3_unified_semseg.pt --source_labelspace milrem $1 $2' > $CHKPT_DIR/inference.sh
+echo 'segment_images --segmenter yolo_generic --model_path yolov3_unified_semseg.pt --source_labelspace milrem $1 $2' > /home/checkpoint_dir/inference.sh
